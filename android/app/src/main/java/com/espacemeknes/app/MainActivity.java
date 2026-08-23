1ca1bc6ffc444e5ec160c75091a314827124679a
faiben <faical.benhaida@gmail.com>
2026-08-23 10:09:25 +0200

    fix: clear WebView cache on Android resume to always load latest from Netlify

--- android/app/src/main/java/com/espacemeknes/app/MainActivity.java
+++ android/app/src/main/java/com/espacemeknes/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.espacemeknes.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            WebView webView = getBridge().getWebView();
+            if (webView != null) {
+                webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+                webView.clearCache(false);
+            }
+        } catch (Exception ignored) {}
+    }
+}
